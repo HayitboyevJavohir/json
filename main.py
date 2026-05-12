11a19e3dc0540cead21447248b74cb833014388c
Javohir <javohir@example.com>
2026-05-12 11:20:23 +0500

    done

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-import json
+# import json
 
-x = 10
-x_json = json.dumps(x)
+# x = 10
+# x_json = json.dumps(x)
 
-ism = "anvar"
-ism_json = json.dumps(ism)
+# ism = "anvar"
+# ism_json = json.dumps(ism)
 
-sonlar = [12, 45, 23, 67]
-sonlar_json = json.dumps(sonlar)
+# sonlar = [12, 45, 23, 67]
+# sonlar_json = json.dumps(sonlar)
 
-print(type(sonlar_json))
+# print(type(sonlar_json))
+
+bemor = {
+    "ism": "Alijon Valiyev",
+    "yosh": 30,
+    "oila": True,
+    "farzandlar": ("Ahmad", "Bonu"),
+    "allergiya": None,
+    "dorilar": [
+        {"nomi": "Analgin", "miqdori": 0.5},
+        {"nomi": "Panadol", "miqdori": 1.2}
+    ]
+}
+
+bemor_json = json.dusomps(bemor)
+
+print(bemor_json)
