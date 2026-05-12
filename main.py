d4a21bde7a086f34fee043550a248c54a4014813
Javohir <javohir@example.com>
2026-05-12 11:11:36 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import json
+
+x = 10
+x_json = json.dumps(x)
+
+ism = "anvar"
+ism_json = json.dumps(ism)
+
+sonlar = [12, 45, 23, 67]
+sonlar_json = json.dumps(sonlar)
+
+print(type(sonlar_json))
